ccf49775c4867fa88c6e10b6cdac51796e2cb9b2
lmx0 <2480602196@qq.com>
2023-01-04 23:39:01 +0800

    Site updated: 2023-01-04 23:39:00

--- lmx0/random.js
+++ lmx0/random.js
@@ -1,1 +1,1 @@
-var posts=["post/16107.html","post/668c.html","post/d7cf.html","post/ec43.html","post/d828.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["post/d812.html","post/9c12.html","post/5409.html","post/ec43.html","post/668c.html","post/9f93.html","post/f7ee.html","post/7ffe.html","post/16107.html","post/d3ff.html","post/d828.html","post/3402.html","post/d7cf.html","post/d86b.html","post/c405.html","post/baed.html","post/5824.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};

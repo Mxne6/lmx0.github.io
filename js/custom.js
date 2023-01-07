887cd9d4195160822556ae65bc4d3d5beaedbca1
lmx0 <2480602196@qq.com>
2023-01-07 19:46:35 +0800

    Site updated: 2023-01-07 19:46:34

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+// 首页即刻短文
 var percentFlag = false; // 节流阀
 function essayScroll() {
   let a = document.documentElement.scrollTop || window.pageYOffset; // 卷去高度
@@ -76,102 +77,6 @@ var lmx0 = {
         document.getElementById("waterfall").classList.add("show");
       }, 500);
   },
-  commentText: function (e) {
-    if (e == "undefined" || e == "null") e = "好棒！";
-    var n = document.getElementsByClassName("el-textarea__inner")[0],
-      t = document.createEvent("HTMLEvents");
-    if (!n) return;
-    t.initEvent("input", !0, !0);
-    var o = replaceAll(e, "\n", "\n> ");
-    (n.value = "> " + o + "\n\n"), n.dispatchEvent(t);
-    var i = document.querySelector("#post-comment").offsetTop;
-    window.scrollTo(0, i - 80),
-      n.focus(),
-      n.setSelectionRange(-1, -1),
-      document.getElementById("comment-tips") && document.getElementById("comment-tips").classList.add("show");
-  },
-};
-
-
-
-var percentFlag = false; // 节流阀
-function essayScroll() {
-  let a = document.documentElement.scrollTop || window.pageYOffset; // 卷去高度
-  const waterfallResult = a % document.documentElement.clientHeight; // 卷去一个视口
-  result <= 99 || (result = 99);
-
-  if (
-    !percentFlag &&
-    waterfallResult + 100 >= document.documentElement.clientHeight &&
-    document.querySelector("#waterfall")
-  ) {
-    // console.info(waterfallResult, document.documentElement.clientHeight);
-    setTimeout(() => {
-      waterfall("#waterfall");
-    }, 500);
-  } else {
-    setTimeout(() => {
-      document.querySelector("#waterfall") && waterfall("#waterfall");
-    }, 500);
-  }
-
-  const r = window.scrollY + document.documentElement.clientHeight;
-
-  let p = document.getElementById("post-comment") || document.getElementById("footer");
-
-  (p.offsetTop + p.offsetHeight / 2 < r || 90 < result) && (percentFlag = true);
-}
-function replaceAll(e, n, t) {
-  return e.split(n).join(t);
-}
-var anzhiyu = {
-  diffDate: function (d, more = false) {
-    const dateNow = new Date();
-    const datePost = new Date(d);
-    const dateDiff = dateNow.getTime() - datePost.getTime();
-    const minute = 1000 * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const month = day * 30;
-
-    let result;
-    if (more) {
-      const monthCount = dateDiff / month;
-      const dayCount = dateDiff / day;
-      const hourCount = dateDiff / hour;
-      const minuteCount = dateDiff / minute;
-
-      if (monthCount >= 1) {
-        result = datePost.toLocaleDateString().replace(/\//g, "-");
-      } else if (dayCount >= 1) {
-        result = parseInt(dayCount) + " " + GLOBAL_CONFIG.date_suffix.day;
-      } else if (hourCount >= 1) {
-        result = parseInt(hourCount) + " " + GLOBAL_CONFIG.date_suffix.hour;
-      } else if (minuteCount >= 1) {
-        result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.date_suffix.min;
-      } else {
-        result = GLOBAL_CONFIG.date_suffix.just;
-      }
-    } else {
-      result = parseInt(dateDiff / day);
-    }
-    return result;
-  },
-  changeTimeInEssay: function () {
-    document.querySelector("#bber") &&
-      document.querySelectorAll("#bber time").forEach(function (e) {
-        var t = e,
-          datetime = t.getAttribute("datetime");
-        (t.innerText = anzhiyu.diffDate(datetime, true)), (t.style.display = "inline");
-      });
-  },
-  reflashEssayWaterFall: function () {
-    document.querySelector("#waterfall") &&
-      setTimeout(function () {
-        waterfall("#waterfall");
-        document.getElementById("waterfall").classList.add("show");
-      }, 500);
-  },
   commentText: function (txt) {
     const postCommentDom = document.querySelector("#post-comment");
     var domTop = postCommentDom.offsetTop;
@@ -221,9 +126,33 @@ var anzhiyu = {
   },
 };
 
+// 导航栏
+// 返回顶部 显示网页阅读进度
+window.onscroll = percent; // 执行函数
+// 页面百分比
+function percent() {
+  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+    b =
+      Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight
+      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
+    result = Math.round((a / b) * 100), // 计算百分比
+    btn = document.querySelector("#percent"); // 获取图标
+
+  result <= 99 || (result = 99), (btn.innerHTML = result);
+}
+
+document.getElementById("page-name").innerText = document.title.split(" | lmx0")[0];
+
+
 
 lmx0.changeTimeInEssay();
 lmx0.reflashEssayWaterFall();
-anzhiyu.initIndexEssay();
-anzhiyu.changeTimeInEssay();
-anzhiyu.reflashEssayWaterFall();
+lmx0.initIndexEssay();
+lmx0.changeTimeInEssay();
+lmx0.reflashEssayWaterFall();

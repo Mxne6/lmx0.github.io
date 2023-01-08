12c521ab6e8bf43ecb4fb0e656ba7d4cd1b87685
lmx0 <2480602196@qq.com>
2023-01-08 21:38:55 +0800

    Site updated: 2023-01-08 21:38:55

--- js/custom.js
+++ js/custom.js
@@ -164,3 +164,56 @@ function switchCommentBarrage () {
   // 本地缓存一天，刷新或切换页面时仍 隐藏或显示 热评。
   window.localStorage.setItem('commentBarrageDisplay', flag === 'false' ? 'undefined' : 'false', 86400000)
 }
+
+
+
+
+//分类条 start
+categoriesBarActive()
+topCategoriesBarScroll()
+
+//分类条
+function categoriesBarActive(){
+  var urlinfo = window.location.pathname;
+  urlinfo = decodeURIComponent(urlinfo)
+  console.log(urlinfo);
+  //判断是否是首页
+  if (urlinfo == '/'){
+    if (document.querySelector('#category-bar')){
+      document.getElementById('首页').classList.add("select")
+    }
+  }else {
+    // 验证是否是分类链接
+    var pattern = /\/categories\/.*?\//;
+    var patbool = pattern.test(urlinfo);
+    console.log(patbool);
+    // 获取当前的分类
+    if (patbool) {
+      var valuegroup = urlinfo.split("/");
+      console.log(valuegroup[2]);
+      // 获取当前分类
+      var nowCategorie = valuegroup[2];
+      if (document.querySelector('#category-bar')){
+        document.getElementById(nowCategorie).classList.add("select");
+      }
+    }
+  }
+  
+}
+
+//鼠标控制横向滚动
+function topCategoriesBarScroll(){
+  if (document.getElementById("category-bar-items")){
+    let xscroll = document.getElementById("category-bar-items");
+  xscroll.addEventListener("mousewheel", function (e) {
+    //计算鼠标滚轮滚动的距离
+    let v = -e.wheelDelta / 2;
+    xscroll.scrollLeft += v;
+    //阻止浏览器默认方法
+    e.preventDefault();
+}, false);
+  }
+}
+// 分类条 end
+
+
--- js/fuzhi.js
+++ js/fuzhi.js
@@ -1,9 +1,8 @@
-/** 监听copy事件 */
+// 监听copy事件
 document.addEventListener("copy",function(e){
     //复制的内容
     btf.snackbarShow('复制成功，请遵循GPL协议', false, 3000)
   })
-  
 
 // 检测按键
 window.onkeydown = function (e) {
@@ -11,3 +10,12 @@ window.onkeydown = function (e) {
       btf.snackbarShow('开发者模式已打开，请遵循GPL协议', false, 3000)
     }
   }
+
+  // 分享本页
+function share() {
+  let url = window.location.origin + window.location.pathname
+  btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+  new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
+  
+}
+

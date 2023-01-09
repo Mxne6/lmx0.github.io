aa43e5d384998b64012bf829ff93c9ed5d418310
lmx0 <2480602196@qq.com>
2023-01-09 16:01:14 +0800

    Site updated: 2023-01-09 16:01:13

--- js/custom.js
+++ js/custom.js
@@ -217,3 +217,41 @@ function topCategoriesBarScroll(){
 // 分类条 end
 
 
+
+// // 网站运行时间 start
+setInterval(() => {
+  let create_time = Math.round(new Date('2022/12/14 00:00:00').getTime() / 1000); //在此行修改建站时间
+  let timestamp = Math.round((new Date().getTime()) / 1000);
+  let second = timestamp - create_time;
+  let time = new Array(0, 0, 0, 0, 0);
+
+  var nol = function(h) {
+      return h > 9 ? h : '0' + h;
+  }
+  if (second >= 365 * 24 * 3600) {
+      time[0] = parseInt(second / (365 * 24 * 3600));
+      second %= 365 * 24 * 3600;
+  }
+  if (second >= 24 * 3600) {
+      time[1] = parseInt(second / (24 * 3600));
+      second %= 24 * 3600;
+  }
+  if (second >= 3600) {
+      time[2] = nol(parseInt(second / 3600));
+      second %= 3600;
+  }
+  if (second >= 60) {
+      time[3] = nol(parseInt(second / 60));
+      second %= 60;
+  }
+  if (second >= 0) {
+      time[4] = nol(second);
+  }
+  let currentTimeHtml = ""
+  if (time[0] != 0) {
+      currentTimeHtml += time[0] + ' YEAR '
+  }
+  currentTimeHtml += "本站已运行：" + time[1] + ' 天 ' + time[2] + ' 时 ' + time[3] + ' 分 ' + time[4] + ' 秒';
+  document.getElementById("runtime").innerHTML = currentTimeHtml;
+}, 1000);
+// // 网站运行时间 end

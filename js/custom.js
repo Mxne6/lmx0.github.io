9eb391dfd4485a42ce26826c18ed8cf3dfeb47f0
lmx0 <2480602196@qq.com>
2023-01-11 23:36:52 +0800

    Site updated: 2023-01-11 23:36:52

--- js/custom.js
+++ js/custom.js
@@ -248,3 +248,82 @@ setInterval(() => {
   document.getElementById("runtime").innerHTML = currentTimeHtml;
 }, 1000);
 // // 网站运行时间 end
+
+
+function switchDarkMode() {
+  "light" == ("dark" === document.documentElement.getAttribute("data-theme") ? "dark" : "light") ? (activateDarkMode(),
+      saveToLocal.set("theme", "dark", 2),
+      void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)) : (activateLightMode(),
+      saveToLocal.set("theme", "light", 2),
+      void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)),
+  "function" == typeof utterancesTheme && utterancesTheme(),
+      "object" === ("undefined" == typeof FB ? "undefined" : _typeof(FB)) && window.loadFBComment(),
+      window.DISQUS && document.getElementById("disqus_thread").children.length && setTimeout(function() {
+          return window.disqusReset()
+      }, 200)
+}
+
+
+// music start------
+var lmx0_musicPlaying = false;
+var lmx0_musicFirst = false;
+var navMusicEl = document.getElementById("nav-music");
+var lmx0 = {
+  //切换音乐播放状态
+  musicToggle: function (changePaly = true) {
+    if (!lmx0_musicFirst) {
+      musicBindEvent();
+      lmx0_musicFirst = true;
+    }
+    let msgPlay = '<i class="fa-solid fa-play"></i><span>播放音乐</span>'; // 此處可以更改為你想要顯示的文字
+    let msgPause = '<i class="fa-solid fa-pause"></i><span>暂停音乐</span>'; // 同上，但兩處均不建議更改
+    if (lmx0_musicPlaying) {
+      navMusicEl.classList.remove("playing");
+      // 修改右键菜单文案为播放
+      // document.getElementById("menu-music-toggle").innerHTML = msgPlay;
+      document.getElementById("nav-music-hoverTips").innerHTML = "音乐已暂停";
+      // document.querySelector("#consoleMusic").classList.remove("on");
+      lmx0_musicPlaying = false;
+      navMusicEl.classList.remove("stretch");
+    } else {
+      navMusicEl.classList.add("playing");
+      // 修改右键菜单文案为暂停
+      // document.getElementById("menu-music-toggle").innerHTML = msgPause;
+      // document.querySelector("#consoleMusic").classList.add("on");
+      lmx0_musicPlaying = true;
+      navMusicEl.classList.add("stretch");
+    }
+    if (changePaly) document.querySelector("#nav-music meting-js").aplayer.toggle();
+  },
+  // 音乐伸缩
+  musicTelescopic: function () {
+    if (navMusicEl.classList.contains("stretch")) {
+      navMusicEl.classList.remove("stretch");
+    } else {
+      navMusicEl.classList.add("stretch");
+    }
+  },
+
+  //音乐上一曲
+  musicSkipBack: function () {
+    document.querySelector("#nav-music meting-js").aplayer.skipBack();
+  },
+
+  //音乐下一曲
+  musicSkipForward: function () {
+    document.querySelector("#nav-music meting-js").aplayer.skipForward();
+  },
+
+  //获取音乐中的名称
+  musicGetName: function () {
+    var x = $(".aplayer-title");
+    var arr = [];
+    for (var i = x.length - 1; i >= 0; i--) {
+      arr[i] = x[i].innerText;
+    }
+    return arr[0];
+  },
+};
+
+addRightMenuClickEvent();
+// music end-----------
--- js/fuzhi.js
+++ js/fuzhi.js
@@ -1,21 +1,31 @@
-// 监听copy事件
-document.addEventListener("copy",function(e){
-    //复制的内容
-    btf.snackbarShow('复制成功，请遵循GPL协议', false, 3000)
-  })
-
-// 检测按键
-window.onkeydown = function (e) {
-    if (e.keyCode === 123) {
-      btf.snackbarShow('开发者模式已打开，请遵循GPL协议', false, 3000)
-    }
-  }
-
-  // 分享本页
+// 分享本页
 function share() {
   let url = window.location.origin + window.location.pathname
   btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
   new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
-  
+}
+// f12
+window.onkeydown = e=>{
+  123 === e.keyCode && btf.snackbarShow("开发者模式已打开，请遵循GPL协议")
 }
 
+document.body.addEventListener("copy", (e=>{
+  "TEXTAREA" == e.target.tagName && "" == e.target.className || btf.snackbarShow("复制成功~")
+}
+))
+
+document.body.addEventListener("paste", (e=>{
+  btf.snackbarShow("粘贴成功~")
+}
+))
+
+
+// 音乐绑定事件
+function musicBindEvent() {
+  document.querySelector("#nav-music .aplayer-music").addEventListener("click", function () {
+    lmx0.musicTelescopic();
+  });
+  document.querySelector("#nav-music .aplayer-button").addEventListener("click", function () {
+    lmx0.musicToggle(false);
+  });
+}

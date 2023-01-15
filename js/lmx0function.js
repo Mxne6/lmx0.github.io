da8297ddd40da42d0be87be40c75e753850baec2
lmx0 <2480602196@qq.com>
2023-01-15 16:13:49 +0800

    Site updated: 2023-01-15 16:13:48

---
+++ js/lmx0function.js
@@ -0,0 +1,1 @@
+function musicBindEvent(){document.querySelector("#nav-music .aplayer-music").addEventListener("click",(function(){lmx0.musicTelescopic()})),document.querySelector("#nav-music .aplayer-button").addEventListener("click",(function(){lmx0.musicToggle(!1)}))}

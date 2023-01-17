4611e035dd85fd02ebd49d4dd8fe3d68c834e155
lmx0 <2480602196@qq.com>
2023-01-18 00:05:36 +0800

    Site updated: 2023-01-18 00:05:35

--- js/lmx0function.js
+++ js/lmx0function.js
@@ -7,3 +7,13 @@ function musicBindEvent() {
       lmx0.musicToggle(false);
     });
   }
+
+
+
+function switchCommentBarrage() {
+  let e = window.localStorage.getItem("commentBarrageDisplay");
+  document.getElementById("comment-barrage").style.zIndex = "false" === e ? "1" : "-1001",
+  window.localStorage.setItem("commentBarrageDisplay", "false" === e ? "undefined" : "false", 864e5),
+  document.getElementById("menu-barrage") && (document.querySelector("#menu-barrage>span").innerHTML = "false" === e ? "隐藏热评" : "显示热评"),
+  document.getElementById("barrage-btn") && ("false" === e ? document.getElementById("barrage-btn").classList.add("on") : document.getElementById("barrage-btn").classList.remove("on"))
+}
